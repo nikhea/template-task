8780947b24c6d39c2711b6114caf156e9511816d
nikhea <imonikheaugbodaga@gmail.com>
2023-01-20 22:20:20 +0100

    updated prodution Url

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+let main = "https://template-task-nikhea.vercel.app/api/templates";
+let test = "http://localhost:3000/api/templates";
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api/templates",
+  baseURL: main,
   // http://localhost:3000/api/templates
 });
 
