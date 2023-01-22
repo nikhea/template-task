b825ea1dccb830aa3b283a20c769f1a3e1d62550
nikhea <imonikheaugbodaga@gmail.com>
2023-01-23 00:16:55 +0100

    added more data

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 // import p from "../pages/api/templates"
-let main = "https://template-task-nikhea.vercel.app/api/templates";
+import a from "../pages/api/templates";
+let main = "/api/templates";
+
+// let main = "https://template-task-nikhea.vercel.app/api/templates";
 let test = "http://localhost:3000/api/templates";
 const instance = axios.create({
   baseURL: main,
